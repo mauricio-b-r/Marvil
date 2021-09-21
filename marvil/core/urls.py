6e34018f4161669fd698092931b54ccc81d8fa82
Mauricio Blanco Regojo <mauricioblancoregojo@gmail.com>
2021-09-20 22:49:40 -0300

    updated urls

--- marvil/core/urls.py
+++ marvil/core/urls.py
@@ -5,11 +5,9 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
+router.register(r"api/users", views.UserViewSet)
+router.register(r"api/groups", views.GroupViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("", include(router.urls)),
     path("api/login/", views.LoginAPI.as_view(), name="login"),
